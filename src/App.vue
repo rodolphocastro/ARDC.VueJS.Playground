3b1c26e773ebcb019509247471ce243f48ba8f31
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-07-29 22:42:49 -0300

    Remover referências ao HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Welcome to ARDC's VueJS Playground Client</h3>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
