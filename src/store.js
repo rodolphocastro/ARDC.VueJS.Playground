165eca77ae5300d00c9771b4458840b2a46dd326
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2019-08-01 07:48:18 -0300

    Adicionar consulta à API de Games

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios'
+
 import router from '@/router.js';
+
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        apiUrl: "http://localhost:5000/",
+        apiUrl: "http://localhost:5000",
         gitHubRepoUrl: "https://github.com/rodolphocastro/ARDC.NetCore.Playground",
         swaggerUiUrl: "http://localhost:5000/index.html",
         swaggerDocUrl: "http://localhost:5000/swagger/v1/swagger.json",
@@ -15,9 +18,19 @@ export default new Vuex.Store({
         }
     },
     mutations: {
-
+        setGames(state, payload){
+            state.games.gamesList = payload;
+        },
     },
     actions: {
-
+        async getGames({state, commit}) {
+            try {
+                let response = await axios.get(`${state.apiUrl}/games`);
+                commit('setGames', response.data);
+            }
+            catch(error){
+                commit('setGames', []);
+            }
+        }
     }
 })
--- src/views/Games.vue
+++ src/views/Games.vue
@@ -5,12 +5,23 @@
         </p>
         <img class="page-logo" alt="a dualshock 4 controller" src="@/assets/games_logo.png">
         <h3>Games</h3>
+        <button @click="showGames()">Load Games</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'games',
+    methods: {
+        showGames(){
+            this.$store.dispatch('getGames');
+        }
+    },
+    computed: {
+        games(){
+            return this.$store.state.games.gamesList;
+        }
+    }
 }
 </script>
 
