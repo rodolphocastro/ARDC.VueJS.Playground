bf44bc2b47766aaa31d4647aed6974dec5595cf6
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-08-11 22:03:14 -0300

    Atualizar visual da tela de criação de jogos

--- src/components/games/GameCreate.vue
+++ src/components/games/GameCreate.vue
@@ -1,17 +1,22 @@
 <template>
-    <div id="game-create">
-        <h3>Create a new game</h3>
-        <form>
-            <label>
-                Title:
-                <input type="text" v-model="newGame.name"/>
-            </label>
-            <label>
-                Release Date:
-                <input type="date" v-model="newGame.releasedOn"/>
-            </label>
-            <button @click.prevent="postGame()">Create</button>
+    <div id="game-create" class="row">
+        <p class="lead col-sm-12">Provide the necessary information:</p>
+        <form class="col-sm-12">
+            <div class="form-group">
+                <label for="inputGameName">Title</label>
+                <input id="inputGameName" class="form-control" type="text" v-model="newGame.name"/>
+            </div>
+            <div class="form-group">
+                <label for="inputGameRelease">Release Date</label>
+                <input id="inputGameRelease" class="form-control" type="date" v-model="newGame.releasedOn"/>
+            </div>
+            <button class="btn btn-primary" @click.prevent="postGame()">Create</button>
         </form>
+        <ul class="col-sm-12 nav justify-content-center">
+            <li class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'gameslist' }">Return to List</router-link>
+            </li>            
+        </ul>
     </div>
 </template>
 
