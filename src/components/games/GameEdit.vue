8fdd58b9d24b500e3e538bced1d18ccf47f6de45
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-08-11 22:09:11 -0300

    Atualizar visual da tela de edição de jogos

--- src/components/games/GameEdit.vue
+++ src/components/games/GameEdit.vue
@@ -1,18 +1,24 @@
 <template>
-    <div id="game-edit" v-if="currentGame">
-        <form>
-            <label>
-                Title:
-                <input type="text" v-model="currentGame.name"/>
-            </label>
-            <br/>
-            <label>
-                Release Date:
-                <input type="datetime-local" v-model="currentGame.releasedOn"/>
-            </label>
-            <br/>
-            <button @click.prevent="saveChanges()">Save Changes</button>
+    <div id="game-edit" class="row" v-if="currentGame">
+        <form class="col-sm-12">
+            <div class="form-group">
+                <label for="inputGameName">Title</label>
+                <input id="inputGameName" class="form-control" type="text" v-model="currentGame.name"/>
+            </div>
+            <div class="form-group">
+                <label for="inputGameRelease">Release Date</label>                
+                <input id="inputGameRelease" class="form-control" type="datetime-local" v-model="currentGame.releasedOn"/>
+            </div>
+            <button class="btn btn-primary" @click.prevent="saveChanges()">Save Changes</button>
         </form>
+        <ul class="col-sm-12 nav justify-content-center">
+            <li class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'gameslist' }">Return to List</router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link" :to="{ name: 'gamedetail', params: { id: currentGame.id } }">Discard Changes</router-link>
+            </li>
+        </ul>
     </div>
 </template>
 
