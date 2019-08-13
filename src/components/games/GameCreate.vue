307818d9c890e2bc5caebae45f96eaa802998690
rodolpho.alves <rodolpho.castro.a@gmail.com>
2019-08-12 21:28:52 -0300

    Refatorar visualização dos components de Games

--- src/components/games/GameCreate.vue
+++ src/components/games/GameCreate.vue
@@ -1,43 +1,53 @@
 <template>
-    <div id="game-create" class="row">
-        <p class="lead col-sm-12">Provide the necessary information:</p>
-        <form class="col-sm-12">
-            <div class="form-group">
-                <label for="inputGameName">Title</label>
-                <input id="inputGameName" class="form-control" type="text" v-model="newGame.name"/>
-            </div>
-            <div class="form-group">
-                <label for="inputGameRelease">Release Date</label>
-                <input id="inputGameRelease" class="form-control" type="date" v-model="newGame.releasedOn"/>
-            </div>
-            <button class="btn btn-primary" @click.prevent="postGame()">Create</button>
-        </form>
-        <ul class="col-sm-12 nav justify-content-center">
-            <li class="nav-item">
-                <router-link class="nav-link" :to="{ name: 'gameslist' }">Return to List</router-link>
-            </li>            
-        </ul>
+  <div id="game-create" class="container">
+    <div class="row">
+      <p class="lead col-sm-12">Provide the necessary information:</p>
     </div>
+    <div class="row">
+      <form class="col-sm-12">
+        <div class="form-group">
+          <label for="inputGameName">Title</label>
+          <input id="inputGameName" class="form-control" type="text" v-model="newGame.name" />
+        </div>
+        <div class="form-group">
+          <label for="inputGameRelease">Release Date</label>
+          <input
+            id="inputGameRelease"
+            class="form-control"
+            type="date"
+            v-model="newGame.releasedOn"
+          />
+        </div>
+        <button class="btn btn-primary" @click.prevent="postGame()">Create</button>
+      </form>
+    </div>
+    <div class="row">
+      <ul class="col-sm-12 nav justify-content-center">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{ name: 'gameslist' }">Return to List</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'gameCreate',
-    methods: {
-        postGame(){
-            this.$store.dispatch('addGame', this.newGame);
-            alert("The game was created");
-        }
-    },
-    data: () => ({
-        newGame: {
-            name: "",
-            releasedOn: "",
-        },
-    })
-}
+  name: "gameCreate",
+  methods: {
+    postGame() {
+      this.$store.dispatch("addGame", this.newGame);
+      alert("The game was created");
+    }
+  },
+  data: () => ({
+    newGame: {
+      name: "",
+      releasedOn: ""
+    }
+  })
+};
 </script>
 
 <style>
-
 </style>
--- src/components/games/GamesList.vue
+++ src/components/games/GamesList.vue
@@ -1,41 +1,37 @@
 <template>
-    <div id="games-list">
-        <ul class="list-group">
-            <li class="list-group-item list-group-item-action" v-for="game in games" :key="game.id" @click="detailGame(game.id)">
-                {{ game.name }}
-            </li>
-        </ul>
-    </div>
+  <div id="games-list" class="container">
+    <ul class="list-group row">
+      <li
+        class="list-group-item list-group-item-action"
+        v-for="game in games"
+        :key="game.id"
+        @click="detailGame(game.id)"
+      >{{ game.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'gamesList',
-    methods: {
-        getGames(){
-            this.$store.dispatch('getGames');
-        },        
-        detailGame(gameId){
-            this.$router.push({ name: 'gamedetail', params: { id: gameId}});
-        }
+  name: "gamesList",
+  methods: {
+    getGames() {
+      this.$store.dispatch("getGames");
     },
-    computed: {
-        games(){
-            return this.$store.getters.games;
-        }
-    },
-    mounted: function () {
-        this.getGames();
+    detailGame(gameId) {
+      this.$router.push({ name: "gamedetail", params: { id: gameId } });
     }
-}
+  },
+  computed: {
+    games() {
+      return this.$store.getters.games;
+    }
+  },
+  mounted: function() {
+    this.getGames();
+  }
+};
 </script>
 
 <style>
-
-#games-table{
-    width: 80%;
-    margin-left: 10%;
-    margin-right: 10%;
-}
-
 </style>
